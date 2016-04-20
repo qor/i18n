f5f6038c2d660cd94a99610869edd5b0a0d4502c
binku <binku87@gmail.com>
2016-04-20 17:39:23 +0800

    inline-edit: make it support default value

--- inline_edit/inline_edit.go
+++ inline_edit/inline_edit.go
@@ -20,16 +20,29 @@ func GenerateFuncMaps(I18n *i18n.I18n, locale string, request *http.Request) tem
 
 func inlineEdit(I18n *i18n.I18n, locale string, isInline bool) func(string, ...interface{}) template.HTML {
 	return func(key string, args ...interface{}) template.HTML {
+		// Get Translation Value
+		var value template.HTML
+		var defaultValue string
+		if len(args) > 0 {
+			if args[0] == nil {
+				defaultValue = key
+			} else {
+				defaultValue = fmt.Sprint(args[0])
+			}
+			value = I18n.Default(defaultValue).T(locale, key, args[1:]...)
+		} else {
+			value = I18n.T(locale, key)
+		}
+
+		// Append inline-edit script/tag
 		if isInline {
 			var editType string
-			value := I18n.T(locale, key, args...)
 			if len(value) > 25 {
 				editType = "data-type=\"textarea\""
 			}
-			assetsTag := fmt.Sprintf("<script data-prefix=\"%v\" src=\"/admin/assets/javascripts/i18n-checker.js?theme=i18n\"></script>", I18n.Resource.GetAdmin().GetRouter().Prefix)
-			return template.HTML(fmt.Sprintf("%s<span class=\"qor-i18n-inline\" %s data-locale=\"%s\" data-key=\"%s\">%s</span>", assetsTag, editType, locale, key, value))
-		} else {
-			return I18n.T(locale, key, args...)
+			assetsTag := fmt.Sprintf("<script data-prefix=\"%v\" src=\"/%v/assets/javascripts/i18n-checker.js?theme=i18n\"></script>", I18n.Resource.GetAdmin().GetRouter().Prefix)
+			return template.HTML(fmt.Sprintf("%s<span class=\"qor-i18n-inline\" %s data-locale=\"%s\" data-key=\"%s\">%s</span>", assetsTag, editType, locale, key, string(value)))
 		}
+		return value
 	}
 }
