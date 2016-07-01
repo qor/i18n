d56ff0f165d673ace5cb8d79171aa925ebe8b36f
binku <binku87@gmail.com>
2016-07-01 14:41:58 +0800

    Add more import testcases

--- exchange_actions/exchange_actions_test.go
+++ exchange_actions/exchange_actions_test.go
@@ -24,9 +24,12 @@ var I18n *i18n.I18n
 
 func init() {
 	db = utils.TestDB()
+	reset()
+}
+
+func reset() {
 	db.DropTable(&database.Translation{})
 	database.New(db)
-
 	Admin := admin.New(&qor.Config{DB: db})
 	Worker = worker.New()
 	Admin.AddResource(Worker)
@@ -38,12 +41,14 @@ func init() {
 	exchange_actions.RegisterExchangeJobs(I18n, Worker)
 }
 
+// Test export translations with scope
 type testExportWithScopedCase struct {
 	Scope            string
 	ExpectExportFile string
 }
 
 func TestExportTranslations(t *testing.T) {
+	reset()
 	testCases := []*testExportWithScopedCase{
 		&testExportWithScopedCase{Scope: "", ExpectExportFile: "export_all.csv"},
 		&testExportWithScopedCase{Scope: "All", ExpectExportFile: "export_all.csv"},
@@ -66,22 +71,53 @@ func TestExportTranslations(t *testing.T) {
 	}
 }
 
+// Test import translations
+type testImportTranslationsCase struct {
+	ImportFileDesc string
+	ImportFile     string
+	ExpectZhValues map[string]string
+}
+
 func TestImportTranslations(t *testing.T) {
-	color.Green(fmt.Sprintf("Import TestCase #%d: Success\n", 1))
-	for _, job := range Worker.Jobs {
-		if job.Name == "Import Translations" {
-			job.Handler(&exchange_actions.ImportTranslationArgument{TranslationsFile: media_library.FileSystem{media_library.Base{Url: "imports/import_translations.csv"}}}, job.NewStruct().(worker.QorJobInterface))
-			zhMapping := map[string]string{"qor_admin.title": "标题", "qor_admin.subtitle": "小标题", "qor_admin.description": "描述", "header.title": "标题"}
-			for key, translation := range I18n.LoadTranslations()["zh-CN"] {
-				if zhMapping[key] != translation.Value {
-					t.Errorf(color.RedString(fmt.Sprintf("\nExchange TestCase #%d: Failure (%s)\n", 1, "zh translations not match")))
+	reset()
+	testCases := []*testImportTranslationsCase{
+		&testImportTranslationsCase{
+			ImportFileDesc: "Normal tranlsation file",
+			ImportFile:     "import_1.csv",
+			ExpectZhValues: map[string]string{"qor_admin.title": "标题", "qor_admin.subtitle": "小标题", "qor_admin.description": "描述", "header.title": "标题"},
+		},
+		&testImportTranslationsCase{
+			ImportFileDesc: "Translation file with missing header.title",
+			ImportFile:     "import_2.csv",
+			ExpectZhValues: map[string]string{"qor_admin.title": "标题", "qor_admin.subtitle": "小标题", "qor_admin.description": "描述"},
+		},
+		&testImportTranslationsCase{
+			ImportFileDesc: "Translation file with empty column",
+			ImportFile:     "import_3.csv",
+			ExpectZhValues: map[string]string{"qor_admin.title": "标题", "qor_admin.subtitle": "小标题", "qor_admin.description": "描述", "header.title": "标题"},
+		},
+	}
+
+	for i, testCase := range testCases {
+		for _, job := range Worker.Jobs {
+			if job.Name == "Import Translations" {
+				job.Handler(&exchange_actions.ImportTranslationArgument{TranslationsFile: media_library.FileSystem{media_library.Base{Url: "imports/" + testCase.ImportFile}}}, job.NewStruct().(worker.QorJobInterface))
+				translations := I18n.LoadTranslations()["zh-CN"]
+				if len(translations) == 0 {
+					t.Errorf(color.RedString(fmt.Sprintf("\nImport TestCase #%d: Failure (%s)\n", i+1, "Doesn't have Zh translations")))
 				}
+				for key, translation := range translations {
+					if testCase.ExpectZhValues[key] != translation.Value {
+						t.Errorf(color.RedString(fmt.Sprintf("\nImport TestCase #%d: Failure (%s)\n", i+1, "Zh translations not match")))
+					}
+				}
+				color.Green(fmt.Sprintf("Import TestCase #%d: Success\n", i+1))
 			}
 		}
 	}
-
 }
 
+// Helper functions
 func clearDownloadDir() {
 	files, _ := ioutil.ReadDir("./public/downloads")
 	for _, f := range files {
