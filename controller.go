7da2256d250f7d6540366e066ba7fa4758aac477
Jinzhu <wosmvp@gmail.com>
2016-12-26 14:43:58 +0800

    Use default HTMLSanitizer from qor utils

--- controller.go
+++ controller.go
@@ -1,11 +1,9 @@
 package i18n
 
 import (
-	"github.com/microcosm-cc/bluemonday"
 	"github.com/qor/admin"
+	"github.com/qor/qor/utils"
 )
-
-var htmlSanitizer = bluemonday.UGCPolicy()
 
 type i18nController struct {
 	*I18n
@@ -17,7 +15,7 @@ func (controller *i18nController) Index(context *admin.Context) {
 
 func (controller *i18nController) Update(context *admin.Context) {
 	form := context.Request.Form
-	translation := Translation{Key: form.Get("Key"), Locale: form.Get("Locale"), Value: htmlSanitizer.Sanitize(form.Get("Value"))}
+	translation := Translation{Key: form.Get("Key"), Locale: form.Get("Locale"), Value: utils.HTMLSanitizer.Sanitize(form.Get("Value"))}
 
 	if err := controller.I18n.SaveTranslation(&translation); err == nil {
 		context.Writer.Write([]byte("OK"))
