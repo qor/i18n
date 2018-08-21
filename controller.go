a7bbc2bf2da4ea7e2e75ee777925683b3afa3e32
sg3des <sg3des@gmail.com>
2018-08-21 19:23:38 +0300

    validate locale

--- controller.go
+++ controller.go
@@ -1,6 +1,8 @@
 package i18n
 
 import (
+	"fmt"
+
 	"github.com/qor/admin"
 )
 
@@ -16,10 +18,24 @@ func (controller *i18nController) Update(context *admin.Context) {
 	form := context.Request.Form
 	translation := Translation{Key: form.Get("Key"), Locale: form.Get("Locale"), Value: form.Get("Value")}
 
+	if !controller.validateLocale(&translation) {
+		context.Writer.WriteHeader(400)
+		fmt.Fprintf(context.Writer, "requested locale '%s' is unexpected", translation.Locale)
+	}
+
 	if err := controller.I18n.SaveTranslation(&translation); err == nil {
 		context.Writer.Write([]byte("OK"))
 	} else {
 		context.Writer.WriteHeader(422)
 		context.Writer.Write([]byte(err.Error()))
 	}
+}
+
+func (controller *i18nController) validateLocale(translation *Translation) (ok bool) {
+	for locale := range controller.LoadTranslations() {
+		if locale == translation.Locale {
+			ok = true
+		}
+	}
+	return
 }
